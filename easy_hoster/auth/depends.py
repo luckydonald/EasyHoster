83db489b0bd4668f087c028c78fe8f4e092cad27
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-02-02 18:15:38 +0100

    auth.depends: Reorder for imports to succeed.

--- easy_hoster/auth/depends.py
+++ easy_hoster/auth/depends.py
@@ -4,15 +4,15 @@ from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from .models import FullUser
+
+OAuthPasswordForm = Annotated[OAuth2PasswordRequestForm, Depends()]
+
+
 from .oauth_password import login, oauth_password
-
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl=oauth_password.url_path_for(login.__name__))
 
 
 Token = Annotated[str, Depends(oauth2_scheme)]
-
-OAuthPasswordForm = Annotated[OAuth2PasswordRequestForm, Depends()]
 
 from .core import get_current_user
 AuthenticatedUser = Annotated[FullUser, Depends(get_current_user)]
