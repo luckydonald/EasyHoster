590799dccedce88b4b53e32cae9e58a4913bb391
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-02-02 20:02:50 +0100

    buckets: Helper function to do file path generation in one place.

--- easy_hoster/buckets/main.py
+++ easy_hoster/buckets/main.py
@@ -56,10 +56,47 @@ async def upload_file(
         data.foo = file.filename
         f.write(meta.model_dump_json())
     # end with
+# end def
 
-    with open(file_location, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+
+class GetFilePaths(NamedTuple):
+    file: Path
+    meta: Path
+# end class
+
+
+async def get_file_paths(
+    bucket: Bucket,
+    file_id: UUID,
+) -> GetFilePaths:
+    folder = UPLOAD_DIR / bucket
+    file_location = folder / f"{file_id}.blob"
+    meta_location = folder / f"{file_id}.meta"
+    if not os.path.exists(file_location):
+        raise HTTPException(status_code=404, detail="File not found on disk")
+    # end if
+    if not os.path.exists(meta_location):
+        raise HTTPException(status_code=404, detail="Metadata not found on disk")
+    # end def
+    return GetFilePaths(file_location, meta_location)
+# end def
+
+
+class GetFileMetadata(NamedTuple):
+    file: Path
+    meta: FileMetadataWithBucket
+# end class
+
+
+async def get_file_metadata(
+    bucket: Bucket,
+    file_id: UUID,
+) -> GetFileMetadata:
+    locations = await get_file_paths(bucket, file_id)
+    with open(locations.meta) as f:
+        data = f.read()
     # end with
+    return GetFileMetadata(locations.file, data)
 # end def
 
 
@@ -69,30 +106,22 @@ async def get_file(
     bucket: Bucket,
     dl: bool = False,
 ):
-    if file_id not in metadata_store:
-        raise HTTPException(status_code=404, detail="File not found")
-
-    file_id = str(uuid.uuid4())
-    folder = UPLOAD_DIR / bucket
-    folder.mkdir(exist_ok=True)
-    file_location = folder / f"{file_id}.blob"
-    meta_location = folder / f"{file_id}.meta"
-
-    if not os.path.exists(file_location) or not os.path.exists(meta_location):
-        raise HTTPException(status_code=404, detail="File not found on disk")
-    # end if
+    locations = await get_file_paths(bucket, file_id)
     if dl:
-        return FileResponse(file_location, media_type='application/octet-stream', filename=metadata_store[file_id].original_name)
+        return FileResponse(locations.file, media_type='application/octet-stream', filename=metadata_store[file_id].original_name)
     else:
-        return FileResponse(file_location)
+        return FileResponse(locations.file)
     # end if
 # end def
 
 
-@bucket.get("/metadata/file_id}")
+@bucket.get("/metadata/{bucket}/{file_id}")
 async def get_metadata(
     file_id: UUID,
+    bucket: Bucket,
+    dl: bool = False,
 ):
+    locations = await get_file_paths(bucket, file_id)
     if file_id not in metadata_store:
         raise HTTPException(status_code=404, detail="Metadata not found")
     # end if
