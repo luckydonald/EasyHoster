c033ef3eaa7c2137f1b1cedaa34a29c1bf8fc535
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-02-02 22:35:11 +0100

    buckets: Extra types for `EffectiveRole`s.

--- easy_hoster/buckets/depends.py
+++ easy_hoster/buckets/depends.py
@@ -8,9 +8,18 @@ from typing import Annotated
 
 from fastapi import UploadFile, File, Depends
 
-from .depends_funcs import now
-
+from .depends_funcs import now, current_user_has_effective_role
+from .models import EffectiveRole
+from ..auth.models import Role, FullUser
 
 UploadedFile = Annotated[UploadFile, File(description="A file read as UploadFile")]
 
 Now = Annotated[datetime, Depends(now)]
+
+# noinspection PyPep8Naming
+def AuthenticatedUserWithEffectiveRole(role: Role | EffectiveRole):
+    return Annotated[FullUser, Depends(current_user_has_effective_role(role))]
+# end def
+
+AuthenticatedUploader = AuthenticatedUserWithEffectiveRole(EffectiveRole.UPLOADER)
+AuthenticatedUnauthenticated = AuthenticatedUserWithEffectiveRole(EffectiveRole.UNAUTHENTICATED)
--- easy_hoster/buckets/depends_funcs.py
+++ easy_hoster/buckets/depends_funcs.py
@@ -1,6 +1,52 @@
+__all__ = (
+    'now',
+    'current_user_has_effective_role',
+)
+
 from datetime import datetime, timezone
+
+from .paths import get_file_metadata
+from ..auth.core import current_user_has_role, error_if_forbidden
+from ..auth.depends import AuthenticatedUser, AuthenticatedUserOrNone
+from ..auth.models import Role
+from .models import EffectiveRole, Bucket, FileId
 
 
 def now() -> datetime:
     return datetime.now(tz=timezone.utc)
+# end def
+
+
+def current_user_has_effective_role(role: Role | EffectiveRole):
+    async def _current_user_has_effective_role_uploader(current_user: AuthenticatedUser, bucket: Bucket, file_id: FileId):
+        info = await get_file_metadata(bucket, file_id)
+        return error_if_forbidden(
+            allowed=current_user.username == info.meta.uploaded_by,
+            role=role,
+            user=current_user,
+        )
+    # end def
+
+    async def _current_user_has_effective_role_unauthenticated(current_user: AuthenticatedUserOrNone):
+        return error_if_forbidden(
+            allowed=current_user is None,
+            role=role,
+            user=current_user,
+        )
+    # end def
+
+    match role:
+        case Role.ADMIN | Role.NORMAL:
+            return current_user_has_role(role)
+        # end case
+        case EffectiveRole.UPLOADER:
+            return _current_user_has_effective_role_uploader
+        # end case
+        case EffectiveRole.UNAUTHENTICATED:
+            return _current_user_has_effective_role_unauthenticated
+        # end case
+        case default:
+            raise ValueError(f"Invalid role: {role}")
+        # end case
+    # end match
 # end def
