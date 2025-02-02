531df9fc26a42077eea18eccdd1542a3d9377ed2
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-02-03 00:18:52 +0100

    auth: Use the `AuthenticatedNormal`/`AuthenticatedAdmin` for guarding routes.

--- easy_hoster/auth/routes.py
+++ easy_hoster/auth/routes.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
 
-from .core import get_user, get_current_user, is_admin
-from .crypt import hash_password, verify_password
+from .core import get_current_user, is_admin
+from .crypt import hash_password
+from .depends import AuthenticatedAdmin, AuthenticatedNormal
 from .depends import AuthenticatedUser
 from .io import user_store
 from .models import FullUser, StoredUser, Role, ApiUser, Password
@@ -16,8 +17,8 @@ auth.include_router(oauth_password)
 async def create_user(
     username: str,
     password: str,
+    current_user: AuthenticatedAdmin,
     role: Role = Role.NORMAL,
-    current_user: FullUser = Depends(get_current_user),
 ):
     if not is_admin(current_user):
         raise HTTPException(
@@ -31,6 +32,7 @@ async def create_user(
     # end if
     hashed_password = hash_password(password)
 
+    # Note: This is not current_user, but the new one.
     new_user = StoredUser(
         password=hashed_password,
         roles=[role],
@@ -41,26 +43,14 @@ async def create_user(
 
 
 @auth.get("/test/normal")
-async def foobar(current_user: dict = Depends(get_current_user)):
-    if current_user["role"] == "normal":
-        return {"message": "Foobar for normal users"}
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Only normal users can access this route"
-        )
-    # end if
+async def foobar(current_user: AuthenticatedNormal):
+    return {"message": "Foobar for normal users", "user": current_user}
 # end def
 
 
 @auth.get("/test/admin")
-async def foobar(current_user: dict = Depends(get_current_user)):
-    if current_user["role"] == "normal":
-        return {"message": "Foobar for normal users"}
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Only normal users can access this route"
-        )
-    # end if
+async def foobar(current_user: AuthenticatedAdmin):
+    return {"message": "Foobar for admin users", "user": current_user}
 # end def
 
 
