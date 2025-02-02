e285c28e473cf603ab874d17977533f57cc083a9
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-02-02 23:06:45 +0100

    buckets: Small type hint.

--- easy_hoster/buckets/depends_funcs.py
+++ easy_hoster/buckets/depends_funcs.py
@@ -8,7 +8,7 @@ from datetime import datetime, timezone
 from .paths import get_file_metadata
 from ..auth.core import current_user_has_role, error_if_forbidden
 from ..auth.depends import AuthenticatedUser, AuthenticatedUserOrNone
-from ..auth.models import Role
+from ..auth.models import Role, FullUser
 from .models import EffectiveRole, Bucket, FileId
 
 
@@ -27,7 +27,9 @@ def current_user_has_effective_role(role: Role | EffectiveRole):
         )
     # end def
 
-    async def _current_user_has_effective_role_unauthenticated(current_user: AuthenticatedUserOrNone):
+    async def _current_user_has_effective_role_unauthenticated(
+        current_user: AuthenticatedUserOrNone,
+    ) -> FullUser:
         return error_if_forbidden(
             allowed=current_user is None,
             role=role,
