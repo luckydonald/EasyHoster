76d8bbfa16f49650e491ef64b957b438bf75f437
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-02-03 14:52:16 +0100

    auth.utils.new_uuids: Make sure we don't get an internal server error (500) if `current_user_has_role` for some reason has a `current_user=None`.

--- easy_hoster/auth/core.py
+++ easy_hoster/auth/core.py
@@ -71,9 +71,9 @@ async def get_current_user(token: Token) -> FullUser:
 def current_user_has_role(role: Role):
     from .depends import AuthenticatedUser
 
-    async def _current_user_has_role(current_user: AuthenticatedUser) -> FullUser:
+    async def _current_user_has_role(current_user: AuthenticatedUser | None) -> FullUser:
         return error_if_forbidden(
-            allowed=role in current_user.roles,
+            allowed=False if current_user is None else role in current_user.roles,
             role=role,
             user=current_user,
         )
