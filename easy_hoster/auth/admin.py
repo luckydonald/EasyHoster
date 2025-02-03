e3a73de74131cdb4777a088c2b0876260feca638
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-02-03 03:39:12 +0100

    auth: Extracted user management to `auth.admin` file.

---
+++ easy_hoster/auth/admin.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, HTTPException
+from starlette import status
+
+from .core import is_admin
+from .crypt import hash_password
+from .depends import AuthenticatedAdmin
+from .io import user_store
+from .models import FullUser, Role, StoredUser, ApiUser
+
+admin = APIRouter()
+
+
+@admin.post("/users", response_model=FullUser, status_code=status.HTTP_201_CREATED)
+async def create_user(
+    username: str,
+    password: str,
+    current_user: AuthenticatedAdmin,
+    role: Role = Role.NORMAL,
+):
+    if username in user_store:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists"
+        )
+    # end if
+    hashed_password = hash_password(password)
+
+    # Note: This is not current_user, but the new one.
+    new_user = StoredUser(
+        password=hashed_password,
+        roles=[role],
+    )
+    user_store[username] = new_user
+    return new_user
+# end def
--- easy_hoster/auth/routes.py
+++ easy_hoster/auth/routes.py
@@ -1,45 +1,17 @@
-from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter
 from starlette import status
 
-from .core import get_current_user, is_admin
 from .crypt import hash_password
 from .depends import AuthenticatedAdmin, AuthenticatedNormal
 from .depends import AuthenticatedUser
-from .io import user_store
-from .models import FullUser, StoredUser, Role, ApiUser, Password
+from .models import ApiUser, Password
 from .oauth_password import oauth_password
+from .admin import admin
+
 
 auth = APIRouter()
 auth.include_router(oauth_password)
-
-@auth.post("/users", response_model=FullUser, status_code=status.HTTP_201_CREATED)
-async def create_user(
-    username: str,
-    password: str,
-    current_user: AuthenticatedAdmin,
-    role: Role = Role.NORMAL,
-):
-    if not is_admin(current_user):
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Only admins can create users"
-        )
-    # end if
-    if username in user_store:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists"
-        )
-    # end if
-    hashed_password = hash_password(password)
-
-    # Note: This is not current_user, but the new one.
-    new_user = StoredUser(
-        password=hashed_password,
-        roles=[role],
-    )
-    user_store[username] = new_user
-    return new_user
-# end def
+auth.include_router(admin)
 
 
 @auth.get("/test/normal")
