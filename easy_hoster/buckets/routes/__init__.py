ae6bb2792f38009f158753dc90b48565eb2bb358
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-02-04 08:54:02 +0100

    buckets.webdav: Added to routes.

--- easy_hoster/buckets/routes/__init__.py
+++ easy_hoster/buckets/routes/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .bucket import buckets
 from .file import files
+from .webdav import webdav
 from ..constants import TAG
 
 api = APIRouter()
 api.include_router(buckets, prefix="", tags=[f"{TAG}.bucket"])
 api.include_router(files, prefix="", tags=[f"{TAG}.file"])
+api.include_router(webdav, prefix="", tags=[f"{TAG}.webdav"])
